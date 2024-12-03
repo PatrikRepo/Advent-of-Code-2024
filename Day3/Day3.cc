66943ebde9593c8b2e9dda9142bba54b2690ba75
PatrikRepo <patrik@fam-repo.se>
2024-12-03 22:35:41 +0100

    small optimization

--- Day3/Day3.cc
+++ Day3/Day3.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
-
-#include <chrono>
+#include <utility>
 
 void parseInput(std::string &data)
 {
@@ -19,60 +18,9 @@ void parseInput(std::string &data)
   input.close();
 }
 
-uint64_t mul(const std::string &data)
+std::pair<uint64_t,uint64_t> mul(const std::string &data)
 {
-	uint64_t result = 0;
-	
-	size_t pos = data.find_first_of("m");
-	
-	while(pos < data.size())
-	{
-		size_t midpos = data.find_first_of(",",pos);
-		size_t endpos = data.find_first_of(")",pos)+1;
-		
-		midpos = (midpos == std::string::npos) ? data.size() : midpos;
-		endpos = (endpos == std::string::npos) ? data.size() : endpos;
-		
-		if(endpos-pos >= 8 && endpos-pos <= 12 && midpos > pos && midpos < endpos && data.substr(pos,4) == "mul(")
-		{
-			unsigned first = 0;
-			unsigned second = 0;
-			bool corrupt = false;
-			for(unsigned i=pos+4; i<midpos; i++)
-			{
-				if(data[i] < '0' || data[i] > '9')
-				{
-					corrupt = true;
-					break;
-				}
-			}
-			if(!corrupt)
-			{
-				first = std::stoi(data.substr(pos+4,midpos-pos-4));
-				for(unsigned i=midpos+1; i<endpos-1; i++)
-				{
-					if(data[i] < '0' || data[i] > '9')
-					{
-						corrupt = true;
-						break;
-					}
-				}
-				if(!corrupt)
-				{
-					second = std::stoi(data.substr(midpos+1,endpos-1-midpos-1));
-					result += first*second;
-				}
-			}
-		}
-		pos = data.find_first_of("m",pos+1);
-	}
-	
-	return result;
-}
-
-uint64_t doMul(const std::string &data)
-{
-	uint64_t result = 0;
+	std::pair<uint64_t,uint64_t> result = {0,0};
 	
 	bool doMul = true;
 	size_t pos = data.find_first_of("md");
@@ -90,7 +38,7 @@ uint64_t doMul(const std::string &data)
 				doMul = false;
 			}
 		}
-		else if(doMul)
+		else
 		{
 			size_t midpos = data.find_first_of(",",pos);
 			size_t endpos = data.find_first_of(")",pos)+1;
@@ -125,7 +73,8 @@ uint64_t doMul(const std::string &data)
 					if(!corrupt)
 					{
 						second = std::stoi(data.substr(midpos+1,endpos-1-midpos-1));
-						result += first*second;
+						result.first += first*second;
+						result.second += (doMul) ? first*second : 0;
 					}
 				}
 			}
@@ -139,18 +88,16 @@ uint64_t doMul(const std::string &data)
 
 int main()
 {
-	uint64_t resultA = 0;
-	uint64_t resultB = 0;
+	std::pair<uint64_t,uint64_t> result;
 
 	std::string data;
 	
   parseInput(data);
 
-	resultA = mul(data);
-	resultB = doMul(data);
+	result = mul(data);
 	
-  std::cout << "resultA: " << resultA << '\n';
-  std::cout << "resultB: " << resultB << std::endl;
+  std::cout << "resultA: " << result.first << '\n';
+  std::cout << "resultB: " << result.second << std::endl;
 
   return 0;
 }
